9e9e27d94ce86d264d1239f4c2737697c9f38fc0
Ash Phythian <ashley.phythian@onthebeach.co.uk>
2017-10-14 17:06:31 +0100

    WIP - broken when adjacent sharps

--- spec/lib/scale_builder_spec.rb
+++ spec/lib/scale_builder_spec.rb
@@ -4,12 +4,25 @@ RSpec.describe ScaleBuilder do
   describe '.build' do
     subject { ScaleBuilder.build(root, mode) }
 
-    let(:root) { 'F' }
-    let(:mode) { 'maj' }
-    let(:diatonic_f_major) { ["F", "G", "A", "Bb", "C", "D", "E", "F"] }
+    context 'F Major' do
+      let(:root) { 'F' }
+      let(:mode) { 'maj' }
+      let(:diatonic_f_major) { ["F", "G", "A", "Bb", "C", "D", "E", "F"] }
 
-    it 'correctly returns a diatonic scale' do
-      expect(subject).to eql(diatonic_f_major)
+      it 'correctly returns a diatonic scale' do
+        expect(subject).to eql(diatonic_f_major)
+      end
     end
+
+    context 'F Minor' do
+      let(:root) { 'F' }
+      let(:mode) { 'min' }
+      let(:diatonic_f_minor) {["F", "G", "Ab", "Bb#", "C", "Db", "D#", "F"]}
+
+      it 'correctly returns a diatonic scale' do
+        expect(subject).to eql(diatonic_f_minor)
+      end
+    end
+
   end
 end
