31d2e3e9676a026804cffe1e727e758fa7495324
Ash Phythian <ashley.phythian@onthebeach.co.uk>
2017-10-20 16:45:53 +0100

    Functional diatonic scales (finally)

--- lib/scale_builder.rb
+++ lib/scale_builder.rb
@@ -2,6 +2,7 @@ class ScaleBuilder
   attr_reader :root, :mode
 
   InvalidRootError = Class.new(StandardError)
+  InvalidModeError = Class.new(StandardError)
 
   def initialize(root, mode)
     @root = root[0].upcase + root[1..-1]
@@ -14,10 +15,10 @@ class ScaleBuilder
 
   def build
     scale[0..-2].each_with_index.map do |note, index|
-      if adjacent_notes(note, index)
-        flattened_sharp[note]
-      else
+      if rotated_notes[index].include?(note)
         note
+      else
+        flats_and_sharps[note]
       end
     end << scale[-1]
   end
@@ -27,6 +28,10 @@ class ScaleBuilder
   end
 
   def scale
+    @scale ||= generate_scale
+  end
+
+  def generate_scale
     current_index = notes.index(convert_root)
     output = [notes[current_index]]
 
@@ -40,33 +45,21 @@ class ScaleBuilder
 
   private
 
-  def adjacent_notes(note, index)
-    (note[0] == scale[index + 1][0] || note[0] == scale[index - 1][0]) &&
-      note.length == 2
-  end
-
   def convert_root
-    if sharpened_flat.include?(root)
-      sharpened_flat[root]
-    elsif notes.include?(root)
+    if notes.include?(root)
       root
     else
       raise InvalidRootError, 'Not a valid root. For example C/C#/Db'
     end
   end
 
-  def sharpened_flat
+  def flats_and_sharps
     {
       'Db' => 'C#',
       'Eb' => 'D#',
       'Gb' => 'F#',
       'Ab' => 'G#',
-      'Bb' => 'A#'
-    }
-  end
-
-  def flattened_sharp
-    {
+      'Bb' => 'A#',
       'C#' => 'Db',
       'D#' => 'Eb',
       'F#' => 'Gb',
@@ -80,7 +73,37 @@ class ScaleBuilder
       major_intervals
     elsif minor_terms.include?(mode)
       minor_intervals
+    else
+      raise InvalidModeError, 'Not a valid mode. Please enter "maj/major/ioinan or min/minor/aeolian'
     end
+  end
+
+  def sharp?(note)
+    note[1] == '#'
+  end
+
+  def flat?(note)
+    note[1] == 'b'
+  end
+
+  def rotated_notes
+    nested_notes.rotate(root_index)
+  end
+
+  def root_index
+    nested_notes.index { |notes| notes.include?(root) }
+  end
+
+  def nested_notes
+    [
+      ['C', 'C#'],
+      ['Db', 'D', 'D#'],
+      ['Eb', 'E'],
+      ['F', 'F#'],
+      ['Gb', 'G', 'G#'],
+      ['Ab', 'A', 'A#'],
+      ['Bb', 'B']
+    ]
   end
 
   def notes
--- spec/lib/scale_builder_spec.rb
+++ spec/lib/scale_builder_spec.rb
@@ -1,26 +1,60 @@
 require 'spec_helper'
+require 'pry'
 
 RSpec.describe ScaleBuilder do
   describe '.build' do
     subject { ScaleBuilder.build(root, mode) }
 
-    context 'F Major' do
-      let(:root) { 'F' }
-      let(:mode) { 'maj' }
-      let(:diatonic_f_major) { ["F", "G", "A", "Bb", "C", "D", "E", "F"] }
+    context 'real scales' do
+      context 'F Major' do
+        let(:root) { 'F' }
+        let(:mode) { 'maj' }
+        let(:diatonic_f_major) { ["F", "G", "A", "Bb", "C", "D", "E", "F"] }
 
-      it 'correctly returns a diatonic scale' do
-        expect(subject).to eql(diatonic_f_major)
+        it 'correctly returns a diatonic scale' do
+          expect(subject).to eql(diatonic_f_major)
+        end
+      end
+
+      context 'F Minor' do
+        let(:root) { 'F' }
+        let(:mode) { 'min' }
+        let(:diatonic_f_minor) { ["F", "G", "Ab", "Bb", "C", "Db", "Eb", "F"] }
+
+        it 'correctly returns a diatonic scale' do
+          expect(subject).to eql(diatonic_f_minor)
+        end
+      end
+
+      context 'F# Minor' do
+        let(:root) { 'F#' }
+        let(:mode) { 'min' }
+        let(:diatonic_f_sharp_minor) { ["F#", "G#", "A", "B", "C#", "D", "E", "F#"] }
+
+        it 'correctly returns a diatonic scale' do
+          expect(subject).to eql(diatonic_f_sharp_minor)
+        end
       end
     end
 
-    context 'F Minor' do
-      let(:root) { 'F' }
-      let(:mode) { 'min' }
-      let(:diatonic_f_minor) {["F", "G", "Ab", "Bb#", "C", "Db", "D#", "F"]}
+    context 'fake scale' do
+      context 'Zb major' do
+        let(:root) { 'Zb' }
+        let(:mode) { 'major' }
 
-      it 'correctly returns a diatonic scale' do
-        expect(subject).to eql(diatonic_f_minor)
+        it 'raises an error' do
+          expect{ subject }.to raise_error(ScaleBuilder::InvalidRootError)
+        end
+      end
+
+      context 'F sharp' do
+        let(:root) { 'F' }
+        let(:mode) { 'sharp' }
+
+        it 'raises an error' do
+          expect{ subject }.to raise_error(ScaleBuilder::InvalidModeError)
+        end
+
       end
     end
 
